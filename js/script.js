badd4eb9f8ba0104f4af44848f25227074b9d1f1
3nonangelum3 <n13LogvynenkoIvan13n@gmail.com>
2022-04-17 23:59:04 +0300

    ending with registration form

--- js/script.js
+++ js/script.js
@@ -4,31 +4,30 @@ let currentPicture = 1;
 //Fucntions
 //EventListers
 //Timers
-let pictureSwitcher = setInterval(()=>{
-    currentPicture = 1 + currentPicture%7;
-    let opacity = 1;
-    let changed = false;
-    const changer = setInterval(()=>{
-        if (changed) 
-        {
-            opacity += 0.01;
-            if (opacity>=1) {
-                clearInterval(changer);
-                changed = false;
-            }
-        } 
-        else 
-        {
-            opacity -= 0.01;
-            if (opacity <= 0)
-            {
-                changed = true;
-                opacity = 0;
-                image.src = `pics/systemPics/registration/${currentPicture}.svg`;
-            }
-        }
-        image.style.opacity = opacity;
-        console.log(opacity);
-    }, 15);
-}, 7000);
+// let pictureSwitcher = setInterval(()=>{
+//     currentPicture = 1 + currentPicture%7;
+//     let opacity = 1;
+//     let changed = false;
+//     const changer = setInterval(()=>{
+//         if (changed) 
+//         {
+//             opacity += 0.01;
+//             if (opacity>=1) {
+//                 clearInterval(changer);
+//                 changed = false;
+//             }
+//         } 
+//         else 
+//         {
+//             opacity -= 0.01;
+//             if (opacity <= 0)
+//             {
+//                 changed = true;
+//                 opacity = 0;
+//                 image.src = `pics/systemPics/registration/${currentPicture}.svg`;
+//             }
+//         }
+//         image.style.opacity = opacity;
+//     }, 15);
+// }, 7000);
 //Mainflow
