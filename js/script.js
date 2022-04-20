00198d5cca95af517733f0d97b00591ef1b47a4b
3nonangelum3 <n13LogvynenkoIvan13n@gmail.com>
2022-04-20 11:26:37 +0300

    reverting
    false branch to make commits

--- js/script.js
+++ js/script.js
@@ -19,45 +19,6 @@ function authorise(user) {
     
 }
 //EventListers
-document.getElementById("signIn").addEventListener("click", async (event) => {
-    event.preventDefault();
-    const mail = document.getElementById("emailL").value;
-    if (mail != "" && passwordLog!="")
-    {
-        let user = {
-            mail: mail.value,
-            password: passwordLog.value
-        };
-        // const userFromServer = 
-        await fetch('php/login.php', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(user)
-        }).then((responce)=>{
-            console.log(responce.body);
-        }).then(()=>{
-
-        });
-        // .then(()=> {
-        //     if (Response == 1){
-        //         console.log("success");
-        //     }
-        //     console.log(JSON.parse(Response));
-        //     console.log(Response);
-        // })
-        // .catch(() => {
-        //     alert("Could not reach server! Check for ");
-        // })
-        // .finally(() => {
-        //     document.getElementById("loginForm").reset();
-        // });
-    }
-    else {
-        alert("Login or password can`t be empty");
-    }
-});
 document.getElementById("signUp").addEventListener("click", (event)=>{
     event.preventDefault();
     let newUser = {
