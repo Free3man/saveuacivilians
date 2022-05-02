3aac87ba74c8200a6f5d7beeabe7bba0dfe1294b
3nonangelum3 <n13LogvynenkoIvan13n@gmail.com>
2022-05-02 13:11:03 +0300

    saving progress

--- js/map.js
+++ js/map.js
@@ -1,39 +1,27 @@
 "use strict";
+class Marker{
+	constructor(container, type) {
+		this.container = container;
+		this.type = type;
+		
+	}
+	
+}
+function mapInit(){ //returns an object which is a map
+	mapboxgl.accessToken = "pk.eyJ1Ijoic3VhYyIsImEiOiJjbDJudDdycXkxNjE1M2lxY3QyOTM3NHV3In0.KkzJGb4qqdkbDH0NequqPA";
+	return new mapboxgl.Map({
+		container: 'map',
+		style: 'mapbox://styles/mapbox/streets-v11',
+		center: [0, 0],
+		zoom: 1
+	});
+}
+function renderMarkers(markers){
+
+}
+
+
 let markers = [];
-mapboxgl.accessToken = "pk.eyJ1Ijoic3VhYyIsImEiOiJjbDJudDdycXkxNjE1M2lxY3QyOTM3NHV3In0.KkzJGb4qqdkbDH0NequqPA";
-const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: [0, 0],
-    zoom: 1
-});
-const geojson = {
-    type: 'FeatureCollection',
-    features: [
-      {
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [-77.032, 38.913]
-        },
-        properties: {
-          title: 'Mapbox',
-          description: 'Washington, D.C.'
-        }
-      },
-      {
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [-122.414, 37.776]
-        },
-        properties: {
-          title: 'Mapbox',
-          description: 'San Francisco, California'
-        }
-      }
-    ]
-};
 geojson.features.forEach(feature => {
     const element = document.createElement("div");
     element.className = "marker";
