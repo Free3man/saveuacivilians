19b151c900b118c3f9df67db1474f24076ae71eb
3nonangelum3 <n13LogvynenkoIvan13n@gmail.com>
2022-05-02 11:54:32 +0300

    progress

---
+++ js/map.js
@@ -0,0 +1,41 @@
+"use strict";
+let markers = [];
+mapboxgl.accessToken = "pk.eyJ1Ijoic3VhYyIsImEiOiJjbDJudDdycXkxNjE1M2lxY3QyOTM3NHV3In0.KkzJGb4qqdkbDH0NequqPA";
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [0, 0],
+    zoom: 1
+});
+const geojson = {
+    type: 'FeatureCollection',
+    features: [
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-77.032, 38.913]
+        },
+        properties: {
+          title: 'Mapbox',
+          description: 'Washington, D.C.'
+        }
+      },
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-122.414, 37.776]
+        },
+        properties: {
+          title: 'Mapbox',
+          description: 'San Francisco, California'
+        }
+      }
+    ]
+};
+geojson.features.forEach(feature => {
+    const element = document.createElement("div");
+    element.className = "marker";
+    markers[markers.length] = new mapboxgl.Marker(element).setLngLat(feature.geometry.coordinates).addTo(map);
+});
