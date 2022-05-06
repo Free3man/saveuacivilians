af8c3ef3cf6113e626e7729528762529b3f8376a
3nonangelum3 <93791126+3nonangelum3@users.noreply.github.com>
2022-05-06 16:38:16 +0200

    save progress

--- js/map.js
+++ js/map.js
@@ -1,62 +1,56 @@
 "use strict";
 let mapboxgl;
-/**
- * Summary. types of Markers
- * 
- * Description. enum filled with different types of Markers.
- * Is created to define a type of a marker not to do it with string literals.
- * Haw such a values as: 
- * * basic(will be shown with out any changes)
- * * transport(/enter what is the difference/)
- */
 class MarkerType{
-	get basic(){
+	static get basic(){
 		return "basic";
 	}
-	get transport(){
+	static get transport(){
 		return "transport";
 	}
+	static get military(){
+		return "military";
+	}
 }
-class Map{
-	constructor(mapSettings){
-		this.container = mapSettings.container;
-		this.style = mapSettings.style;
-		this.center = mapSettings.center;
-		this.zoom = mapSettings.zoom;
+class Feature{
+	constructor(coordinates, title, description){
+		this.coordinates = coordinates;
+		this.title = title;
+		this.description = description;
 	}
-	get map(){
-		return this.map;
+	toObject(){
+		return {
+			type: "Feature",
+			geometry: {
+				type: 'Point',
+				coordinates: this.coordinates 
+			},
+			properties:{
+				title: this.title,
+				description: this.description
+			}
+		};
 	}
-	init(){
-		mapboxgl.accessToken = "pk.eyJ1Ijoic3VhYyIsImEiOiJjbDJudDdycXkxNjE1M2lxY3QyOTM3NHV3In0.KkzJGb4qqdkbDH0NequqPA";
-		this.map = new mapboxgl.Map({
-			container: this.container,
-			style: this.style,
-			center: this.center,
-			zoom: this.zoom
-		});
-	}
-	static init(mapSettings){
-		mapboxgl.accessToken = "pk.eyJ1Ijoic3VhYyIsImEiOiJjbDJudDdycXkxNjE1M2lxY3QyOTM3NHV3In0.KkzJGb4qqdkbDH0NequqPA";
-		return new mapboxgl.Map({
-			container: mapSettings.container,
-			style: mapSettings.style,
-			center: mapSettings.center,
-			zoom: mapSettings.zoom
-		});
+	static toObject(feature){
+		return {
+			type: "Feature",
+			geometry: {
+				type: 'Point',
+				coordinates: feature.coordinates 
+			},
+			properties:{
+				title: feature.title,
+				description: feature.description
+			}
+		};
 	}
 }
 class Marker{
-	/**
-	 * 
-	 * @param {tag} container 
-	 * @param {} type 
-	 * @param {*} feature 
-	 */
-	constructor(container, type, feature) {
+	constructor(container, type, feature, map) {
 		this.container = container;
 		this.type = type;
 		this.feature = feature;
+		this.render(map);
+		map.markers[map.markers.length()] = this;
 	}
 	toJSON(){
 		return JSON.stringify({
@@ -86,8 +80,98 @@ class Marker{
 			feature: marker.feature
 		};
 	}
+	static toMarker(data, map){
+		return new Marker(data.container, data.type, data.feature, map);
+	}
 	render(Map){
-		Map.markers[Map.markers.length] = new mapboxgl.Marker(this.container)
-		.setLngLat(this.feature.geometry.coordinates).addTo(Map.map);
+		Map.markers[this.id] = new mapboxgl.Marker(this.container)
+		.setLngLat(this.feature.coordinates).addTo(Map.map);
+		this.id = Map.map.markers.length();
 	}
 }
+class Map{
+	constructor(container, style, center, zoom){
+		this.container = container;
+		this.style = style;
+		this.center = center;
+		this.zoom = zoom;
+		this.init();
+	}
+	get map(){
+		return this.map;
+	}
+	get markers(){
+		return this.markers;
+	}
+	init(){
+		mapboxgl.accessToken = "pk.eyJ1Ijoic3VhYyIsImEiOiJjbDJudDdycXkxNjE1M2lxY3QyOTM3NHV3In0.KkzJGb4qqdkbDH0NequqPA";
+		this.map = new mapboxgl.Map({
+			container: this.container,
+			style: this.style,
+			center: this.center,
+			zoom: this.zoom
+		});
+	}
+	static init(mapSettings){
+		mapboxgl.accessToken = "pk.eyJ1Ijoic3VhYyIsImEiOiJjbDJudDdycXkxNjE1M2lxY3QyOTM3NHV3In0.KkzJGb4qqdkbDH0NequqPA";
+		return new mapboxgl.Map({
+			container: mapSettings.container,
+			style: mapSettings.style,
+			center: mapSettings.center,
+			zoom: mapSettings.zoom
+		});
+	}
+	getMarkers(){
+		fetch("php/map.php", {
+			method: "GET",
+		}).then((response)=>{
+			if (response.ok && response.status == 200) {
+				response.arrayBuffer();
+			}
+		}).then((answer)=>{
+			answer.forEach(item => {
+				Marker.toMarker(item, this);
+			});
+		}).catch(()=>{
+
+		}).finally(()=>{
+
+		});
+	}
+	getMap(){
+
+	}
+	setMarkers(){
+
+	}
+	setMap(){
+
+	}
+	static getMarkers(map){
+		fetch("php/markers.php", {
+			method: "GET",
+		}).then((response)=>{
+			if (response.ok && response.status == 200) {
+				response.arrayBuffer();
+			}
+		}).then((answer)=>{
+			answer.forEach(item => {
+				Marker.toMarker(item, map);
+			});
+		}).catch(()=>{
+
+		}).finally(()=>{
+
+		});
+	}
+	static getMap(){
+
+	}
+	static setMarkers(){
+		
+	}
+	static setMap(){
+
+	}
+}
+//https://youtu.be/89gnG9ErOaA
--- php/map.php
+++ php/map.php
@@ -1,3 +1,4 @@
 <?
+    require_once("php/connect.php");
     
 ?>
