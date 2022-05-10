682b7f82041714f5cf1f284224fc7fe0677939af
Free3man <94737722+Free3man@users.noreply.github.com>
2022-05-10 20:48:32 +0300

    just fucking push after month of work

--- js/script.js
+++ js/script.js
@@ -1,27 +1,46 @@
-// variables
-const iconLinks = document.querySelectorAll(".icon-link"),
-      sectionFormLinks = document.querySelectorAll(".icon-section img"),
-      contentForm = document.querySelector(".content-box"),
-      sideBarPC = document.querySelector(".icons-side-bar-pc"),
-      headerMobile = document.querySelector(".mobile-profile-bar"),
-      footerMobile = document.querySelector(".footer-side-bar-mobile"),
-      sectionPage = document.querySelector(".section_page"),
-      rules = ["Вкажіть заголовок для залучення уваги волонтерів та детально опишіть вид необхідної допомоги.", "Якщо характер допомоги є гумунітарним: збір продовольства або медична допомога, то задля відстежування прогресу вкажіть усі необхідні ресурси у таблиці.", "Оберіть вашу локацію (місце збору гуманітарної допомоги, зустрічі для перевезення).", "Оберіть дату та час завершення заявки.", "Напишіть у цій секції додаткові контактні дані, розкажіть більш детально про вашу організацію (необов’язково)."];
 
+// navigation-menu --variables
+const iconLinks = document.querySelectorAll(".iconLink"),
+      sectionFormLinks = document.querySelectorAll(".icon-section img"),
+      contentForm = document.querySelector(".contentBox"),
+      sideBarPC = document.querySelector(".sideBar"),
+      headerMobile = document.querySelector(".mobileProfileBar"),
+      footerMobile = document.querySelector(".mobileFooter");
+// --functions
+iconLinks.forEach(iconLink => {
+    iconLink.addEventListener("click", function() {
+        iconLinks.forEach(linkRemove => linkRemove.classList.remove("active"));
+        this.classList.add("active");
+    });
+});
+// css-fix
+const sectionPage = document.querySelector(".sectionRequestForm");
+window.addEventListener("load", () =>
+{
+    sectionPage.style.width = `calc(100vw - ${sideBarPC.offsetWidth}px)`;
+    sectionPage.style.height = `calc(100vh - ${(headerMobile.clientHeight + footerMobile.clientHeight)}px)`;
+});
+
+const iconLinkFormSet = document.querySelectorAll(".mobileIconForm"),
+      iconLinkFormSetMobile = document.querySelectorAll(".mobilelinksForm svg"),
+      hintParagraph = document.querySelectorAll(".hintParagraph"),
+      buttonSwitch = document.querySelector(".control-box button"),
+      inputTableAdd = document.querySelector(".add-next-field-input"),
+      tableForm = document.querySelector(".data_form_staff"),
+      inputAddBlock = document.querySelector("#add_field_type"),
+      selectCheckbox = document.querySelector(".fieldCheckBoxActivity"),
+      svgCircleProgress = document.querySelector(".circle_box circle:nth-child(2)"),
+      rules = ["Вка&#173;жіть за&#173;го&#173;ло&#173;вок для за&#173;лу&#173;чен&#173;ня ува&#173;ги во&#173;лон&#173;терів та де&#173;таль&#173;но опи&#173;шіть вид не&#173;об&#173;хід&#173;ної до&#173;по&#173;мо&#173;ги.", "Як&#173;що ха&#173;рак&#173;тер до&#173;по&#173;мо&#173;ги є гу&#173;мунітар&#173;ним, то зад&#173;ля від&#173;сте&#173;жу&#173;ван&#173;ня про&#173;гре&#173;су збо&#173;ру вка&#173;жіть усі не&#173;об&#173;хід&#173;ні ре&#173;сур&#173;си у таб&#173;ли&#173;ці.", "Оберіть ва&#173;шу ло&#173;ка&#173;цію (міс&#173;це збо&#173;ру гу&#173;манітар&#173;ної до&#173;по&#173;мо&#173;ги, зустрічі для пе&#173;ре&#173;ве&#173;зен&#173;ня).",  "На&#173;пи&#173;шіть у цій сек&#173;ції до&#173;дат&#173;ко&#173;ві кон&#173;такт&#173;ні дані, роз&#173;ка&#173;жіть більш де&#173;таль&#173;но про ва&#173;шу ор&#173;гані&#173;за&#173;цію (не&#173;обов’яз&#173;ко&#173;во).", "Оберіть да&#173;ту та час за&#173;вер&#173;шен&#173;ня за&#173;яв&#173;ки.", ""];
 // objects
 const formData = {
     title: "",
     mainInfo: "",
-    typeOfWork: "",
+    typeOfWork: [],
     table: {
-        field1:{
-            stuff: "",
-            number: 0,
-            units: "", 
-        }
+
     },
     adressLine: "",
-    timer: Date(),
+    timer: "",
     extraInfo: "",
     accept: {
         line1: false,
@@ -31,36 +50,129 @@ const formData = {
 
 //addEventListener
 
-window.addEventListener("load", () =>
-{
-    sectionPage.style.height = `calc(100vh - ${(headerMobile.clientHeight + footerMobile.clientHeight)}px)`;
-})
 
-iconLinks.forEach(iconLink => {
+
+
+iconLinkFormSet.forEach(iconLink => {
     iconLink.addEventListener("click", function() {
-        iconLinks.forEach(linkRemove => linkRemove.classList.remove("active"));
-        this.classList.add("active");
+        console.log(hintParagraph);
+        iconLinkFormSet.forEach(iconLinkFormRemove => iconLinkFormRemove.children[0].classList.remove("active"));
+        this.children[0].classList.add("active");
+        let numSection = parseInt(this.getAttribute('data-formSection')),
+            numberFilled = 0;
+        if(numSection != 5)
+        {
+            hintParagraph.forEach(hint => {hint.innerHTML = '<b style="font-weight: 600;">Підказка: </b>' +rules[numSection];})
+            hintParagraph.forEach(hint => {
+                hint.classList.add("animate__flipInX");
+            });
+        }
+        else
+        {
+            hintParagraph.forEach(hint => {hint.innerHTML = '';});  
+        }
+        for (let field in formData) {
+            if((field!="")&&(field!=[]))
+            {
+                numberFilled++;
+            }
+        }
+        // svgCircleProgress.style.cssText = `stroke-dashoffset: calc((100 * 6) - ((100 * 6) * (${numberFilled} * 10)) / 100)`;
+        insertForm(numSection);
     });
 });
+inputAddBlock.addEventListener("change", function(){
+    selectCheckbox.innerHTML += `<div class="activity">
+        <input type="checkbox" name="" id="transit_auto"><label for="transit_auto">${this.value}</label>
+        <img src="/img/deleteBin.svg" alt=""class="deleteRecord">
+    </div>`;
+    for(recordBin of document.getElementsByClassName("deleteRecord"))
+    {   
+        recordBin.addEventListener("click", function(){
+            this.parentElement.remove();
+            if(selectCheckbox.children.length <= 6)
+            {
+                inputAddBlock.removeAttribute('disabled', '');
+                inputAddBlock.style.opacity = "1";
+            }
+        });
+    }
+    if(selectCheckbox.children.length == 6)
+    {
+        this.setAttribute('disabled', '');
+        this.style.opacity = "0.5";
+    }
+    this.value = "";
+});
+// animation
+hintParagraph.forEach(hint => {hint.addEventListener('animationend', () => {hint.classList.remove("animate__flipInX");});});
+contentForm.addEventListener('animationend', () => {contentForm.classList.remove("animate__fadeIn");});
+buttonSwitch.addEventListener("click", () => {buttonSwitch.classList.add("animate__jello");});
+buttonSwitch.addEventListener("animationend", () => {buttonSwitch.classList.remove("animate__jello");});
 
 
-// sectionPage.style.height = `calc(100vh - ${sideBarPC.offsetWidth}px)`;
 
 
-// sectionFormLinks.forEach(formLink => {
-//     formLink.addEventListener("click", function() {
+//functions
+function insertForm(idSection)
+{
+    contentForm.classList.remove("animate__fadeIn");
+    for(formSection of contentForm.children)
+    {
+        formSection.style.display="none";
+    }
+    contentForm.children[idSection].style.display="flex";
+    contentForm.classList.add("animate__fadeIn");
+}
+
+
+//form executor
+
+const titleBox = document.getElementById("title_box_form"),
+      mainInfoBox = document.getElementById("main_info_box_form"),
+      dropDownMenu = document.querySelector(".taskTitleBox"),
+      selectorTitle  = document.querySelector(".taskTitleBox"),
+      selectorButton = document.querySelector(".taskTitleBox img"),
+      selectorList = document.querySelector(".activitiesList");
+
+    
+
+//create element
+document.querySelector(".taskTitleBox").addEventListener("click", () => {
+    document.querySelector(".taskTitleBox img").classList.toggle("active");  
+    document.querySelector(".activitiesList").classList.toggle("active");
+});
+
+inputTableAdd.addEventListener("change", function () {
+    if(this.value != "")
+    {
+        let clonetableRow = this.parentElement.parentElement.cloneNode(true),
+            newRow = clonetableRow.firstElementChild.firstElementChild,
+            rowElements = this.parentElement.parentElement.children;
+        newRow.value = this.value;
+        newRow.classList.remove("add-next-field-input");
+        newRow.addEventListener("change", checkBlank);
+        for (rowElement of rowElements)
+        {
+            this.parentElement.parentElement.children[0].firstElementChild.value = "";
+            this.parentElement.parentElement.children[1].firstElementChild.value = "";
+            this.parentElement.parentElement.children[2].firstElementChild.value = "";
+        }
         
-//     });
-// });
+        tableForm.appendChild(clonetableRow);
+    }
 
-// contentForm.innerHTML = 
-// `
-// <input type="text" name="" placeholder="Адреса">
-// <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d29925.971825435387!2d35.06043173705367!3d48.47365731806246!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2sua!4v1650455497288!5m2!1sru!2sua" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-// `;
+});
 
-// contentForm.innerHTML = 
-// `
-// <input type="text" name="" placeholder="Адреса">
-// <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d29925.971825435387!2d35.06043173705367!3d48.47365731806246!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2sua!4v1650455497288!5m2!1sru!2sua" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-// `;
+function checkBlank(){
+    if(this.value == "")
+    {
+        this.parentElement.parentElement.remove();
+    }
+}
+const contentFormFields = document.getElementsByClassName("contentFields");
+
+for (fieldForm of contentFormFields) {
+    for(childrenField of fieldForm.children)
+    console.log(fieldForm.children);
+}
