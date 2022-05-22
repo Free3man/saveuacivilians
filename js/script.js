79e3f3447c40212b60a56b2de85aba584ae7a554
Free3man <94737722+Free3man@users.noreply.github.com>
2022-05-22 19:12:45 +0300

    Form and volunteering page stylization

--- js/script.js
+++ js/script.js
@@ -1,61 +1,52 @@
+const iconLinks = document.querySelectorAll(".iconLink");
+const pageSections = document.querySelectorAll("main > section");
+class Navigation {
+    constructor(links, pages) {
+        this.links = links;
+        this.pages = pages;
+    }
+    changePage () {
+        this.links.forEach(linkAdd => {
+            linkAdd.addEventListener("click", (e) => {
+                for (let linkRemove of this.links) {
+                   linkRemove.classList.remove("active");
+                }
+                e.target.parentElement.classList.add("active");
+                for (let pageHide of this.pages) {
+                    pageHide.style = "display: none;";
+                }
+                let pageID = parseInt(e.target.parentElement.getAttribute("data-page"));
+                this.pages[pageID].style = "display: block;";
+            });
+        });
+    }
+}
 
-// navigation-menu --variables
-const iconLinks = document.querySelectorAll(".iconLink"),
-      sectionFormLinks = document.querySelectorAll(".icon-section img"),
-      contentForm = document.querySelector(".contentBox"),
-      sideBarPC = document.querySelector(".sideBar"),
-      headerMobile = document.querySelector(".mobileProfileBar"),
-      footerMobile = document.querySelector(".mobileFooter");
+window.onload = function () {
+    const navMotion = new Navigation(iconLinks, pageSections);
+    navMotion.changePage();
+};
 // --functions
-iconLinks.forEach(iconLink => {
-    iconLink.addEventListener("click", function() {
-        iconLinks.forEach(linkRemove => linkRemove.classList.remove("active"));
-        this.classList.add("active");
-    });
-});
-// css-fix
-const sectionPage = document.querySelector(".sectionRequestForm");
-window.addEventListener("load", () =>
-{
-    sectionPage.style.width = `calc(100vw - ${sideBarPC.offsetWidth}px)`;
-    sectionPage.style.height = `calc(100vh - ${(headerMobile.clientHeight + footerMobile.clientHeight)}px)`;
-});
 
+const sideBarPC = document.querySelector(".sideBar");
+const headerMobile = document.querySelector(".mobileProfileBar");
+const footerMobile = document.querySelector(".mobileFooter");
+
+const contentForm = document.querySelector(".contentBox");
 const iconLinkFormSet = document.querySelectorAll(".mobileIconForm"),
       iconLinkFormSetMobile = document.querySelectorAll(".mobilelinksForm svg"),
       hintParagraph = document.querySelectorAll(".hintParagraph"),
-      buttonSwitch = document.querySelector(".control-box button"),
+      buttonSwitch = document.querySelectorAll(".control-box button"),
       inputTableAdd = document.querySelector(".add-next-field-input"),
       tableForm = document.querySelector(".data_form_staff"),
       inputAddBlock = document.querySelector("#add_field_type"),
       selectCheckbox = document.querySelector(".fieldCheckBoxActivity"),
-      svgCircleProgress = document.querySelector(".circle_box circle:nth-child(2)"),
       rules = ["Вка&#173;жіть за&#173;го&#173;ло&#173;вок для за&#173;лу&#173;чен&#173;ня ува&#173;ги во&#173;лон&#173;терів та де&#173;таль&#173;но опи&#173;шіть вид не&#173;об&#173;хід&#173;ної до&#173;по&#173;мо&#173;ги.", "Як&#173;що ха&#173;рак&#173;тер до&#173;по&#173;мо&#173;ги є гу&#173;мунітар&#173;ним, то зад&#173;ля від&#173;сте&#173;жу&#173;ван&#173;ня про&#173;гре&#173;су збо&#173;ру вка&#173;жіть усі не&#173;об&#173;хід&#173;ні ре&#173;сур&#173;си у таб&#173;ли&#173;ці.", "Оберіть ва&#173;шу ло&#173;ка&#173;цію (міс&#173;це збо&#173;ру гу&#173;манітар&#173;ної до&#173;по&#173;мо&#173;ги, зустрічі для пе&#173;ре&#173;ве&#173;зен&#173;ня).",  "На&#173;пи&#173;шіть у цій сек&#173;ції до&#173;дат&#173;ко&#173;ві кон&#173;такт&#173;ні дані, роз&#173;ка&#173;жіть більш де&#173;таль&#173;но про ва&#173;шу ор&#173;гані&#173;за&#173;цію (не&#173;обов’яз&#173;ко&#173;во).", "Оберіть да&#173;ту та час за&#173;вер&#173;шен&#173;ня за&#173;яв&#173;ки.", ""];
-// objects
-const formData = {
-    title: "",
-    mainInfo: "",
-    typeOfWork: [],
-    table: {
-
-    },
-    adressLine: "",
-    timer: "",
-    extraInfo: "",
-    accept: {
-        line1: false,
-        line2: false
-    }
-};
-
-//addEventListener
-
 
 
 
 iconLinkFormSet.forEach(iconLink => {
     iconLink.addEventListener("click", function() {
-        console.log(hintParagraph);
         iconLinkFormSet.forEach(iconLinkFormRemove => iconLinkFormRemove.children[0].classList.remove("active"));
         this.children[0].classList.add("active");
         let numSection = parseInt(this.getAttribute('data-formSection')),
@@ -66,16 +57,14 @@ iconLinkFormSet.forEach(iconLink => {
             hintParagraph.forEach(hint => {
                 hint.classList.add("animate__flipInX");
             });
+            buttonSwitch[1].style="display:none"; 
+            buttonSwitch[0].style="display:block"; 
         }
         else
         {
-            hintParagraph.forEach(hint => {hint.innerHTML = '';});  
-        }
-        for (let field in formData) {
-            if((field!="")&&(field!=[]))
-            {
-                numberFilled++;
-            }
+            hintParagraph.forEach(hint => {hint.innerHTML = '';}); 
+            buttonSwitch[0].style="display:none"; 
+            buttonSwitch[1].style="display:block"; 
         }
         // svgCircleProgress.style.cssText = `stroke-dashoffset: calc((100 * 6) - ((100 * 6) * (${numberFilled} * 10)) / 100)`;
         insertForm(numSection);
@@ -86,7 +75,7 @@ inputAddBlock.addEventListener("change", function(){
         <input type="checkbox" name="" id="transit_auto"><label for="transit_auto">${this.value}</label>
         <img src="/img/deleteBin.svg" alt=""class="deleteRecord">
     </div>`;
-    for(recordBin of document.getElementsByClassName("deleteRecord"))
+    for(var recordBin of document.getElementsByClassName("deleteRecord"))
     {   
         recordBin.addEventListener("click", function(){
             this.parentElement.remove();
@@ -107,8 +96,8 @@ inputAddBlock.addEventListener("change", function(){
 // animation
 hintParagraph.forEach(hint => {hint.addEventListener('animationend', () => {hint.classList.remove("animate__flipInX");});});
 contentForm.addEventListener('animationend', () => {contentForm.classList.remove("animate__fadeIn");});
-buttonSwitch.addEventListener("click", () => {buttonSwitch.classList.add("animate__jello");});
-buttonSwitch.addEventListener("animationend", () => {buttonSwitch.classList.remove("animate__jello");});
+buttonSwitch[0].addEventListener("click", () => {buttonSwitch[0].classList.add("animate__jello");});
+buttonSwitch[0].addEventListener("animationend", () => {buttonSwitch[0].classList.remove("animate__jello");});
 
 
 
@@ -129,19 +118,21 @@ function insertForm(idSection)
 //form executor
 
 const titleBox = document.getElementById("title_box_form"),
-      mainInfoBox = document.getElementById("main_info_box_form"),
-      dropDownMenu = document.querySelector(".taskTitleBox"),
-      selectorTitle  = document.querySelector(".taskTitleBox"),
-      selectorButton = document.querySelector(".taskTitleBox img"),
-      selectorList = document.querySelector(".activitiesList");
+    labelTrigerMenu = document.querySelectorAll(".labelTrigerMenu"),
+    selectorList = document.querySelector(".activitiesList");
 
     
 
 //create element
-document.querySelector(".taskTitleBox").addEventListener("click", () => {
-    document.querySelector(".taskTitleBox img").classList.toggle("active");  
-    document.querySelector(".activitiesList").classList.toggle("active");
-});
+console.log(labelTrigerMenu);
+for (let selectors of labelTrigerMenu) {
+    selectors.addEventListener("click", function() {
+        console.log(this.children[1]);
+        this.children[1].classList.toggle("active");  
+        this.parentElement .children[1].classList.toggle("active");
+    });
+}
+
 
 inputTableAdd.addEventListener("change", function () {
     if(this.value != "")
@@ -176,3 +167,132 @@ for (fieldForm of contentFormFields) {
     for(childrenField of fieldForm.children)
     console.log(fieldForm.children);
 }
+
+
+
+
+
+
+//form components
+const formSubmitBtn = document.querySelector(".volunteeringformSendButton"),
+      svgCircleProgress = document.querySelector(".circular-chart > .circle"),
+      svgCirclePercentage = document.querySelector(".circular-chart > .percentage"),
+      titleInfoBox = document.getElementById("title_box_form"),
+      mainInfoBox = document.getElementById("main_info_box_form"),
+      adressInfoBox = document.getElementById("adress_info_box_form"),
+      acceptPolicy = document.querySelectorAll("#accept_form > input[type=checkbox]"),
+      typesofWorkCheckbox = document.querySelectorAll(".fieldCheckBoxActivity > .activity input[type=checkbox]"),
+      extraInfoBox = document.getElementById("extra_info_box_form");
+var taskProgress = 0;
+//functions of bar animation
+async function increment(indexProgress) {
+    let sec = 1;
+    let promise = new Promise(resolve => {
+        svgCircleProgress.style.setProperty('--value', `${(indexProgress+1)*10}`);
+        let decrementInterval = setInterval(function() { 
+            if(sec<=10) {
+                svgCirclePercentage.innerHTML = `${(indexProgress*10) + sec}%`;
+            }
+            else {
+                clearInterval(decrementInterval);
+                resolve();
+            }
+            sec++;
+        }, 100);
+    });
+    promise.then(() => {
+        indexProgress++;
+        taskProgress = indexProgress;
+    });
+}
+async function decrement(indexProgress) {
+    let sec = 1;
+    let promise = new Promise(resolve => {
+        svgCircleProgress.style.setProperty('--value', `${(indexProgress-1)*10}`);
+        let decrementInterval = setInterval(function() { 
+            if(sec<=10) {
+                svgCirclePercentage.innerHTML = `${(indexProgress*10) - sec}%`;
+            }
+            else {
+                clearInterval(decrementInterval);
+                resolve();
+            }
+            sec++;
+        }, 100);
+    });
+    promise.then(() => {
+        indexProgress--;
+        taskProgress = indexProgress;
+    });
+} 
+// elements validaton
+for (var fieldAccept of acceptPolicy) {
+    fieldAccept.addEventListener("click", function() {
+        if(this.checked) {
+            increment(taskProgress);
+        }
+        else {
+            decrement(taskProgress);
+        }
+    });
+}
+for (var checkBoxWork of typesofWorkCheckbox) {
+    checkBoxWork.addEventListener("click", function() {
+        for (var workChecked of typesofWorkCheckbox) {
+            if(workChecked.checked) {
+                typesofWorkValid = true;
+            }
+        }
+        if(typesofWorkValid) {
+            increment(taskProgress);
+        }
+        else {
+            decrement(taskProgress);
+        }
+    });
+}
+[titleInfoBox, mainInfoBox, extraInfoBox, adressInfoBox].forEach(elementFormVolunteer => elementFormVolunteer.addEventListener("change", function() {   
+    if(this.value.length >= 1) {
+        if(this.getAttribute("data-filled") != "true") {
+            this.setAttribute("data-filled", "true");
+            increment(taskProgress);
+        }
+    }
+    if(this.value.length == 0) {
+        if(this.getAttribute("data-filled") != "false") {
+            this.setAttribute("data-filled", "false");
+            decrement(taskProgress);
+        }
+    }
+}));
+//form sending
+formSubmitBtn.addEventListener("click", () => {
+    const formData = {
+        title: "",
+        mainInfo: "",
+        typeOfWork: [],
+        table: {
+            
+        },
+        adressLine: "",
+        timer: "",
+        extraInfo: "",
+        accept: {
+            line1: false,
+            line2: false
+        }
+    };
+    formData.title = titleInfoBox.value;
+    formData.mainInfo = mainInfoBox.value;
+    formData.extraInfo = extraInfoBox.value;
+    formData.adressLine =  adressInfoBox.value;
+    formData.accept.line1 = acceptPolicy[0].checked;
+    formData.accept.line2 = acceptPolicy[1].checked;
+    for (var checkWork of typesofWorkCheckbox)
+    {
+        if(checkWork.checked) {
+            formData.typeOfWork.push(checkWork.id);
+        }
+    }
+    console.log(formData);
+});
