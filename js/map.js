cbee0f96a0c5283f1353a7822e5108f9e0013563
3nonangelum3 <93791126+3nonangelum3@users.noreply.github.com>
2022-05-30 19:00:55 +0200

    Update map.js

--- js/map.js
+++ js/map.js
@@ -17,7 +17,11 @@ types = {
 	point: "Point"
 };
 //variables
-let geojson = [],
+const accessToken = "pk.eyJ1Ijoic3VhYyIsImEiOiJjbDJudDdycXkxNjE1M2lxY3QyOTM3NHV3In0.KkzJGb4qqdkbDH0NequqPA",
+defaultSearchSettings = {
+	"language": "uk"
+};
+let geojsons = [],
 markers = [],
 map;
 //functions
@@ -35,13 +39,39 @@ function toObject(marker) {
 		feature: marker.feature
 	};
 }
-function convertIntoGeocoding(input){
-	return `https://api.mapbox.com/geocoding/v5/mapbox.places/${input.replace(" ", "%20")}
-	.json?access_token=pk.eyJ1Ijoic3VhYyIsImEiOiJjbDJudDdycXkxNjE1M2lxY3QyOTM3NHV3In0.KkzJGb4qqdkbDH0NequqPA`;
+function convertIntoGeocoding(search, searchSettings){
+	function checkSearchKeyValidity(key) {
+		["bbox", "country","language", "limit"].forEach(item => {
+			if(item == key){
+				return true;
+			}
+		});
+		return false;
+	}
+	let request = `${search.replace(" ", "%20")}.json?`;
+	if (searchSettings.hasOwnProperty("limit")){
+		if(searchSettings.limit>10){
+			searchSettings.limit = 10;
+		}
+		if (searchSettings.limit<0){
+			searchSettings.limit = 5;
+		}
+	}
+	for (const key in searchSettings) {
+		if (searchSettings.hasOwnProperty(key) && checkSearchKeyValidity(key)){
+			request += `${key}=${searchSettings.key}&`;
+		}
+	}
+	// for (const key in defaultSearchSettings) {
+	// 	if (defaultSearchSettings.hasOwnProperty(key) && !searchSettings.hasOwnProperty(key)) {
+	// 		request += `${key}=${defaultSearchSettings.key}&`;
+	// 	}
+	// }
+	return `https://api.mapbox.com/geocoding/v5/mapbox.places/${request}access_token=${accessToken}`;
 }
 //API
 function mapInit(container, style, center, zoom) {
-	mapboxgl.accessToken = "pk.eyJ1Ijoic3VhYyIsImEiOiJjbDJudDdycXkxNjE1M2lxY3QyOTM3NHV3In0.KkzJGb4qqdkbDH0NequqPA";
+	mapboxgl.accessToken = accessToken;
 	map = new mapboxgl.Map({
 		container: container,
 		style: style,
@@ -50,7 +80,7 @@ function mapInit(container, style, center, zoom) {
 	});
 }
 function createGeoJson(coordinates, title) {
-	return {
+	const geojsonData = {
 		type: types.geojson,
 		data: {
 			type: types.feature,
@@ -63,21 +93,23 @@ function createGeoJson(coordinates, title) {
 			}
 		}
 	};
+	geojsons[geojsons.length] = geojsonData;
+	return geojsonData;
 }
 function renderMarker(coordinates) {
 	const element = document.createElement("div");
 	element.className = "marker";
 	markers[markers.length] = new mapboxgl.Marker(element).setLngLat(coordinates).addTo(map);
 }
-function searching(input){
-	let data;
-	fetch(convertIntoGeocoding(input), {
+function searching(search, searchSettings){
+	// let data;
+	fetch(convertIntoGeocoding(search, searchSettings), {
 		method: "GET"
 	}).then(responce => responce.json()
 	).then(answer => {
 		console.log(answer);
 	}).catch(answer => console.log(answer));
-	return data;
+	// return data;
 }
 function getMarkers() {
 	fetch("php/markers.php", {
@@ -103,4 +135,9 @@ window.addEventListener("load", ()=>{
 //Mainflow
 mapInit("map", styles.streets, [75, 50], 2);
 // getMarkers();
-console.log(searching("Dnipro"));
+// console.log(searching("Dnipro"));
+console.log(searching("Dnipro", {
+	bbox: [2,2,2,2],
+	country: "UA",
+	limit: 3,
+}));
