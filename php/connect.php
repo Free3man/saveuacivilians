8d93ccb8a04a5d6906cf4e9cc55f58af2627f144
3nonangelum3 <93791126+3nonangelum3@users.noreply.github.com>
2022-05-31 21:35:36 +0200

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -1,6 +1,6 @@
 <?
     require_once("enums.php");
-    $connection = mysqli_connect("127.0.0.1", "root", "", "caveuacivilsdb");
+    $connection = mysqli_connect("127.0.0.1", "root", "", "saveuacivilsdb");
     if ($connection == false){
         echo json_encode(Responses::$connetionFailed);
         exit();
