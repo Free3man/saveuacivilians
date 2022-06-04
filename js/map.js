140b934dd3a5892afccbe2898a8e0e8f959d7f83
3nonangelum3 <93791126+3nonangelum3@users.noreply.github.com>
2022-06-04 17:21:23 +0200

    Update map.js

--- js/map.js
+++ js/map.js
@@ -11,6 +11,17 @@ styles = {
 	streets: "mapbox://styles/mapbox/streets-v11",
 	light: "mapbox://styles/mapbox/light-v10",
 },
+territoryRegions = {
+	country: "country",
+	region: "region",
+	postcode: "postcode",
+	district: "district",
+	place: "place",
+	locality: "locality",
+	neighborhood: "neighborhood",
+	address: "address",
+	poi: "poi"
+},
 types = {
 	geojson: "geojson",
 	feature: "Feature",
@@ -18,10 +29,11 @@ types = {
 };
 //variables
 const accessToken = "pk.eyJ1Ijoic3VhYyIsImEiOiJjbDJudDdycXkxNjE1M2lxY3QyOTM3NHV3In0.KkzJGb4qqdkbDH0NequqPA",
+searchProperties = ["autocomplete", "bbox", "country", "fuzzyMatch", 
+			"language", "limit", "proximity", "routing", "types", "worldview"],
 defaultSearchSettings = {
-	"language": "uk"
-},
-possibleKeys = ["bbox", "country","language", "limit"];
+	"language" : "uk"
+};
 let geojsons = [],
 markers = [],
 map;
@@ -51,15 +63,16 @@ function convertIntoGeocoding(search, searchSettings){
 		}
 	}
 	for (const key in searchSettings) {
-		if (searchSettings.hasOwnProperty(key)){
+		if (searchSettings.hasOwnProperty(key) && searchProperties.includes(key)){
 			request += `${key}=${searchSettings[key]}&`;
 		}
 	}
-	// for (const key in defaultSearchSettings) {
-	// 	if (defaultSearchSettings.hasOwnProperty(key) && !searchSettings.hasOwnProperty(key)) {
-	// 		request += `${key}=${defaultSearchSettings.key}&`;
-	// 	}
-	// }
+	for (const key in defaultSearchSettings) {
+		if (defaultSearchSettings.hasOwnProperty(key) && !searchSettings.hasOwnProperty(key) &&
+																	searchProperties.includes(key)) {
+			request += `${key}=${defaultSearchSettings[key]}&`;
+		}
+	}
 	return `https://api.mapbox.com/geocoding/v5/mapbox.places/${request}access_token=${accessToken}`;
 }
 //API
@@ -96,20 +109,20 @@ function renderMarker(coordinates) {
 	element.className = "marker";
 	markers[markers.length] = new mapboxgl.Marker(element).setLngLat(coordinates).addTo(map);
 }
-function getMarkers() {
+function getGeojsons() {
 	fetch("php/markers.php", {
 		method: "POST",
 		body: JSON.stringify({zero: 0})
 	}).then((response)=>{
 		if (response.ok && response.status == 200) {
-			console.log(response.text());
+			console.log(response.arrayBuffer());
 		}
 	}).then((answer)=>{
-		markers = answer;
-	}).catch(()=>{
-
+		answer.forEach(item => createGeoJson(item));
+	}).catch((answer)=>{
+		console.error(answer);
 	}).finally(()=>{
-
+		
 	});
 }
 
@@ -124,7 +137,12 @@ window.addEventListener("load", ()=>{
 	document.getElementsByClassName("mapboxgl-ctrl-bottom-right")[0].remove();
 	document.getElementsByClassName("mapboxgl-ctrl-bottom-left")[0].remove();
 });
+//place where you will be writing your code using my api
 document.addEventListener("DOMContentLoaded",  async () => {
-	//Mainflow
+	//mainflow
 	mapInit("map", styles.streets, [35, 47.5], 5);
+	searching("Dnipro", {
+		country: "UA",
+	}).then(answer => answer.features.forEach(item => renderMarker(item.center)));
+	console.log(markers);
 });
