e9bd998b9169261c28abd078e1d7c15a2efcf2f3
3nonangelum3 <93791126+3nonangelum3@users.noreply.github.com>
2022-06-06 13:03:18 +0200

    Update saveGeojson.php

--- php/saveGeojson.php
+++ php/saveGeojson.php
@@ -1,9 +1,11 @@
 <?php
     require_once("connect.php");
     $geojsons = mysqli_query($connection, "SELECT * FROM `geojson`");
+    $postData = file_get_contents('php://input');
+    $post = json_decode($postData, true);
     while ($line = mysqli_fetch_assoc($geojsons)) {
         for ($i=0; $i < mysqli_num_rows(); $i++) { 
-            # code...
+            
         }
     }
 ?>
